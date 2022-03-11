ecb00deff30e75f117314af4665a5e207335472c
Renata Souza <renatta_c@outlook.com>
2022-03-10 21:16:19 -0300

    Aula04: 'Exercicio: Café!'

---
+++ cafe.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var opcao = 2
+     var resultado = when (opcao){
+         1 -> "Café puro"
+         2 -> "Café com Leite"
+         else -> "Erro"
+     }
+     println(resultado)
+ }
