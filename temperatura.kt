4135c903cfa43e2aa9fdfb71e26afdcec6576ff2
Renata Souza <renatta_c@outlook.com>
2022-03-17 20:41:31 -0300

    Aula 4:'Exercicio de decisão usando temperatura'

---
+++ temperatura.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var temp1:Double = -1.0
+    var temp2:Double = 3.0
+    var temp3:Double = 4.0
+    
+    if(temp1<0 || temp2<0 || temp3<0){
+    print("Uma das temperaturas é negativa")
+    
+    }else{
+    print("Todas as temperaturas são positivas")
+
+}
+}
