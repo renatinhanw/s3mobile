2852e740a08118b5340c830bc60b5d9242032034
Renata Souza <renatta_c@outlook.com>
2022-03-17 20:38:08 -0300

    Aula4:'Exercício de decisão composta (if, else if, else)'

---
+++ decisaoComposta.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var idade = 18    
+    if( idade < 14 ){
+        println("Criança") }
+        
+    else if( idade>=14 && idade < 18 ){
+        println("Adolescente")
+    }else{    
+    println("Adulto")
+    
+}
+}
