70029a00c41f51ac791a6e7b3ec5555c440285b3
Renata Souza <renatta_c@outlook.com>
2022-03-17 20:30:14 -0300

    Aula 4: 'Exercício de soma'

---
+++ soma.kt
@@ -0,0 +1,7 @@
+fun main() {
+ /* Somar (+) Subtrair (-) Multiplicar (*) Divisao (/) Módulo (%) Resto de um divisão */
+    var numero1 = 202
+    var numero2 = 5    
+    var resultado = numero1 + numero2
+    println( resultado )
+    }
