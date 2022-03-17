fcb5cb50420ffcf4808042f4d057a241d5f6cec6
Renata Souza <renatta_c@outlook.com>
2022-03-17 20:14:33 -0300

    Aula 5: Exemplo de subalgoritmos

---
+++ subalgoritmos.kt
@@ -0,0 +1,20 @@
+fun main() {
+	var nome = "Fabio"
+	imprimir(nome)
+	val soma = somar(2, 3)
+imprimir("Soma: $soma")
+
+	}
+
+// recebe uma string e não retorna nada (Unit)
+
+fun imprimir(s: String): Unit {
+	println(s)
+}
+
+// Recebe 2 inteiros e retorna uma inteiro
+
+fun somar(a: Int, b: Int): Int {
+    return a + b
+
+}
